a3e6b1e7e0ea1118708e8f9c0323e25b4d5e2730
Nikita K <vrolok15@yandex.ru>
2017-05-24 13:19:05 +0300

    Single Responsibility Principle example

---
+++ SOLIDDesignPrinciplesPractice/SOLIDDesignPrinciplesPractice/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDDesignPrinciplesPractice
+{
+    public class Journal
+    {
+        //Only manages entries
+
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //memento pattern
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+    }
+
+    public class Persistance
+    {
+        //Only saves files
+
+        public void SaveToFile(Journal j, string fileName, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, j.ToString()); 
+            }
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("Sad journal entry...");
+            j.AddEntry("Stupid journal entry...");
+            Console.WriteLine(j);
+
+            var p = new Persistance();
+            var filename = @"c:\temp\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+        }
+    }
+}
